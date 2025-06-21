3d3a1490cba297ab92964275699e65f0bb248e38
JorgeMabuie <jorgemaabuie@gmail.com>
2025-06-21 20:10:19 +0200

    Update roleMiddleware.js

--- Backend/middleware/roleMiddleware.js
+++ Backend/middleware/roleMiddleware.js
@@ -1,5 +1,11 @@
-module.exports = (requiredRole) => (req, res, next) => {
-  if (!req.user || req.user.role !== requiredRole)
+// middleware/roleMiddleware.js
+module.exports = (required) => (req, res, next) => {
+  const userRole = req.user.role;
+  // Permite passar uma string ou array de strings
+  const allowed = Array.isArray(required) ? required : [required];
+
+  if (!req.user || !allowed.includes(userRole)) {
     return res.status(403).json({ error: 'Permissão negada' });
+  }
   next();
 };
