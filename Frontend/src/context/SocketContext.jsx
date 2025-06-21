da17f7f037e243b887569cfeeeb0e7f97db3ec4f
JorgeMabuie <jorgemaabuie@gmail.com>
2025-06-21 19:22:49 +0200

    Create SocketContext.jsx

---
+++ Frontend/src/context/SocketContext.jsx
@@ -0,0 +1,24 @@
+// src/context/SocketContext.jsx
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+
+const SocketContext = createContext(null);
+export const useSocket = () => useContext(SocketContext);
+
+export function SocketProvider({ children }) {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const sock = io(import.meta.env.VITE_API_URL);
+    setSocket(sock);
+    return () => {
+      sock.disconnect();
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
+  );
+}
