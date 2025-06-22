cbb5347f954132c80b63cd864859fe0cb72218ec
JorgeMabuie <jorgemaabuie@gmail.com>
2025-06-22 20:33:39 +0200

    Update Device.js

--- Backend/models/Device.js
+++ Backend/models/Device.js
@@ -1,6 +1,7 @@
+// models/Device.js
 const mongoose = require('mongoose');
 const DeviceSchema = new mongoose.Schema({
-  tenantId: { type: mongoose.Schema.Types.ObjectId, ref: 'Tenant' },
+  tenantId: { type: mongoose.Schema.Types.ObjectId, ref: 'Tenant', required: true },
   imei:     { type: String, required: true, unique: true },
   label:    { type: String },
   status:   { type: String, enum: ['active','blocked'], default: 'active' },
@@ -8,6 +9,6 @@ const DeviceSchema = new mongoose.Schema({
     center: { lat: Number, lng: Number },
     radius: Number
   },
-  owner:    { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null } // ← novo
+  owner:    { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null }
 });
 module.exports = mongoose.model('Device', DeviceSchema);
